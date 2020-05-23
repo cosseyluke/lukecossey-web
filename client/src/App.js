84acde465111cce99076620e66a63fd56523d69b
Luke Cossey <cosseyluke@gmail.com>
2020-05-23 22:17:49 +1200

    styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      apiResponse: "",
-    }
-  }
+import { PostIndex } from './Post';
 
-  callAPI() {
-    fetch("http://localhost:9000/testAPI")
-      .then(res => res.text())
-      .then(res => this.setState({apiResponse: res}))
-      .catch(err => err)
-  }
 
-  componentDidMount() {
-    this.callAPI()
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <p className="App-intro">
-          {this.state.apiResponse}
-        </p>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/log">
+          <PostIndex />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       apiResponse: "",
+//     }
+//   }
+//
+//   callAPI() {
+//     fetch("http://localhost:9000/testAPI")
+//       .then(res => res.text())
+//       .then(res => this.setState({apiResponse: res}))
+//       .catch(err => err)
+//   }
+//
+//   componentDidMount() {
+//     this.callAPI()
+//   }
+//
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//         </header>
+//         <p className="App-intro">
+//           {this.state.apiResponse}
+//         </p>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- client/src/Post.js
+++ client/src/Post.js
@@ -1,18 +1,63 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import { format } from 'date-fns'
+import isodate from 'isodate'
 import './Post.scss';
+
+
+class Post extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      objUrl: `/log/${this.props.post.slug}`
+    }
+  }
+
+  getUrl() {
+    return `/log/${this.props.post.slug}`
+  }
+
+  render () {
+    const pubDate = format(isodate(this.props.post.pub_date), 'MMMM d, y')
+
+    return (
+      <div className="post-listing listing-default" key={this.key}>
+        <div className="post-date-wrap">{pubDate}</div>
+        <div className="text-wrap">
+          <div className="title-wrap">
+            <h4><Link to={this.getUrl()}>{this.props.post.title}</Link></h4>
+            <p className="date">{pubDate}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+class PostList extends Component {
+  render() {
+    return (
+      <div className="post-list">
+        {this.props.posts.map((post) => (
+          <Post post={post} key={post._id} />
+        ))}
+      </div>
+    )
+  }
+}
 
 class PostIndex extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      apiResponse: "",
+      posts: [],
     }
   }
 
   callAPI() {
     fetch("http://localhost:9000/posts")
-      .then(res => res.text())
-      .then(res => this.setState({apiResponse: res}))
+      .then(res => res.json())
+      .then(res => {console.log('res', res.posts); this.setState({posts: res})})
       .catch(err => err)
   }
 
@@ -21,11 +66,13 @@ class PostIndex extends Component {
   }
 
   render() {
+    const posts = this.state.posts
+
     return (
-      <div className="App">
-        <p className="App-intro">
-          {this.state.apiResponse}
-        </p>
+      <div className="post-index-page">
+        <div className="post-list-wrap">
+          <PostList posts={posts} />
+        </div>
       </div>
     );
   }
