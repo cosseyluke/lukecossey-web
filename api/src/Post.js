5ec74a793632574f2ebab5ab9c554a50732fcb8e
Luke Cossey <cosseyluke@gmail.com>
2020-12-05 15:54:17 +0900

    ..

--- api/src/Post.js
+++ api/src/Post.js
@@ -37,7 +37,7 @@ const postSchema = new mongoose.Schema({
     type: String,
     unique: true,
     slug: "title",
-    slugOn: { update: false }
+    permanent: true
   },
   intro: {
     type: String,
