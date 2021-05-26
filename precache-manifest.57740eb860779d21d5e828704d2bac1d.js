e770937519a196c90e7b44e06bad002cb2cca1f2
Luke Cossey <cosseyluke@gmail.com>
2021-05-26 13:34:25 +0900

    Updates

---
+++ precache-manifest.57740eb860779d21d5e828704d2bac1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f2bf039d1dce2c38ee6facf0531362",
+    "url": "/lukecossey-web/index.html"
+  },
+  {
+    "revision": "c73361594e6702c9eb79",
+    "url": "/lukecossey-web/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "8b5f1baebaec813f8bcd",
+    "url": "/lukecossey-web/static/css/main.dbcaa308.chunk.css"
+  },
+  {
+    "revision": "c73361594e6702c9eb79",
+    "url": "/lukecossey-web/static/js/2.1f34967d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lukecossey-web/static/js/2.1f34967d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b5f1baebaec813f8bcd",
+    "url": "/lukecossey-web/static/js/main.dfec8943.chunk.js"
+  },
+  {
+    "revision": "77ae5749115172309002",
+    "url": "/lukecossey-web/static/js/runtime-main.ecf8903a.js"
+  }
+]);
