24839e60c93537a9038396e9e84895d28b183ecb
Luke Cossey <cosseyluke@gmail.com>
2021-06-01 12:35:33 +0900

    Updates

---
+++ precache-manifest.2b4f3213f94bde5da86af12f41b7f86d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b167bd8596f4e0d5dccb9d497ae93327",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db9bd99b0bed798de44d",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b90b7274f5c3e83cbc9a",
+    "url": "/static/css/main.7a829081.chunk.css"
+  },
+  {
+    "revision": "db9bd99b0bed798de44d",
+    "url": "/static/js/2.027cd63d.chunk.js"
+  },
+  {
+    "revision": "9abfb1f15ab4dda9027a2e43a8a1e874",
+    "url": "/static/js/2.027cd63d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b90b7274f5c3e83cbc9a",
+    "url": "/static/js/main.5bef5273.chunk.js"
+  },
+  {
+    "revision": "5eaf30875934c9f1ea2f",
+    "url": "/static/js/runtime-main.420c03f5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://lukecossey.com/precache-manifest.ec1bda116fd96a9a2c4d5b8afe4b8b7d.js"
+  "/precache-manifest.2b4f3213f94bde5da86af12f41b7f86d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://lukecossey.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
